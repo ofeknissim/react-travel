5ed55de3c2f58c32080f1fb440dd9daca3275f4b
Ofek Nissim <ofeknba@gmail.com>
2021-09-07 20:09:33 +0300

    first commit

---
+++ src/components/tourData.js
@@ -0,0 +1,40 @@
+export const tourData =  [
+    {
+        id: 1,
+        city: "new york",
+        img: "./img/newYork.jpeg",
+        name: " new york city",
+        info:
+        "Times Square, New York, United States"
+
+    },
+    {
+        id: 2,
+        city: "paris",
+        img: "./img/paris.jpeg",
+        name: " paris",
+        info:
+        "Eiffel Tower, Paris, France"
+
+    },
+    {
+        id: 3,
+        city: "london",
+        img: "./img/london.jpeg",
+        name: " london",
+        info:
+        "London, United Kingdom"
+
+    },
+    {
+        id: 4,
+        city: "tokyo",
+        img: "./img/tokyo.jpeg",
+        name: " london",
+        info:
+        "Tokyo, Japan"
+
+    }
+    
+
+]
